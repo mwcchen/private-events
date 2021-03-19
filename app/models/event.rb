20983a65dd470ac0ae0d8597aec558fb5962e239
mwcchen <33730138+mwcchen@users.noreply.github.com>
2021-03-19 15:39:40 -0400

    Replaced class methods with scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,12 +3,15 @@ class Event < ApplicationRecord
   has_many :attendees, through: :attendances, source: :attendee
   belongs_to :creator, class_name: "User"
 
-  def self.past
-    return Event.where("date < :now", { now: Date.today })
-  end
+  scope :past, -> { where("date < :now", { now: Date.today }) }
+  scope :upcoming, -> { where("date > :now", { now: Date.today }) }
 
-  def self.upcoming
-    return Event.where("date > :now", { now: Date.today })
-  end
+  # def self.past
+  #   return Event.where("date < :now", { now: Date.today })
+  # end
+  #
+  # def self.upcoming
+  #   return Event.where("date > :now", { now: Date.today })
+  # end
 
 end
